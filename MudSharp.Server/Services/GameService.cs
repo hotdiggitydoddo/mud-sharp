20039fbfc724be1526d8963653be8e3ada89f737
Ryan Dodd <ryandodd@canvaspeople.com>
2018-10-12 16:42:19 -0700

    Add GameService.

---
+++ MudSharp.Server/Services/GameService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MudSharp.Server.Services
+{
+    public class GameService : BackgroundService
+    {
+        private readonly ILogger<GameService> _logger;
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogDebug($"GracePeriodManagerService is starting.");
+
+            stoppingToken.Register(() =>
+                _logger.LogDebug($" GracePeriod background task is stopping."));
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogDebug($"GracePeriod task doing background work.");
+
+                // This eShopOnContainers method is querying a database table 
+                // and publishing events into the Event Bus (RabbitMS / ServiceBus)
+                
+                //CheckConfirmedGracePeriodOrders();
+
+                await Task.Delay(100, stoppingToken);
+            }
+
+            _logger.LogDebug($"GracePeriod background task is stopping.");
+
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            await Task.CompletedTask;
+            // Run your graceful clean-up actions
+        }
+    }
+}
